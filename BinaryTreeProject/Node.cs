362e925e0e86ea59ebdf8a95b9acb2c49738493f
remy delmas <remy.cool@hotmail.fr>
2017-06-13 22:19:45 +0200

    Exo tour de Hanoi Exo Arbre Binaire

---
+++ BinaryTreeProject/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeProject
+{
+    public class Node<T>
+    {
+        public T Value { get; set; }
+
+        public Node<T> LeftChild { get; set; }
+
+        public Node<T> RightChild { get; set; }
+
+        public Node(T value, Node<T> node)
+        {
+            Value = value;
+        }
+
+        
+
+
+    }
+}
---
+++ HanoiTowerProject/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanoiTowerProject
+{
+    class Program
+    {
+
+        static int nbCoup = 0;
+
+        static void Main(string[] args)
+        {
+           
+            Deplacer(4, 1, 2, 3);
+           
+
+            Console.WriteLine($"La tour a été constituée en {nbCoup} coups");
+            Console.ReadLine();
+        }
+
+
+        public static void Deplacer(int diskCount, int fromPole, int toPole, int viaPole)
+        {
+            if (diskCount == 1)
+            {
+                Console.WriteLine("Move disk from pole " + fromPole + " to pole " + toPole);
+                nbCoup++;
+            }
+            else
+            {
+                Deplacer(diskCount - 1, fromPole, viaPole, toPole);
+                Deplacer(1, fromPole, toPole, viaPole);
+                Deplacer(diskCount - 1, viaPole, toPole, fromPole);
+            }
+        }
+
+
+    }
+}
