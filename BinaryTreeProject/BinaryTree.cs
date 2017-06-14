8427594289f8fcf7fc9317646b9d54d31e39d945
remy delmas <remy.cool@hotmail.fr>
2017-06-14 15:38:44 +0200

    BinaryTree:
    - Creation de la structure
    - Recherche prefixe, postfixe et infixe

--- BinaryTreeProject/BinaryTree.cs
+++ BinaryTreeProject/BinaryTree.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BinaryTreeProject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,116 @@ using System.Threading.Tasks;
 
 namespace BinaryTreeProject
 {
-    public class BinaryTree
+    public class BinaryTree<T> where T : IComparable
     {
-        List<Node> Noeuds;
+        public Node<T> Root { get; set; }
+       
+        public BinaryTree(Node<T> root)
+        {
+            Root = root;
+        }
 
+        public void AddNode(Node<T> root, Node<T> nodeToAdd)
+        {
+           
+            if (nodeToAdd.Value.CompareTo(root.Value) < 0)
+            {
+                if (root.LeftChild != null)
+                    AddNode(root.LeftChild, nodeToAdd);
+                else
+                {
+                    root.LeftChild = nodeToAdd;
+                    root.LeftChild.Display("Left", root.Value);
+                }
+            }
+            else
+            {
+                if (nodeToAdd.Value.CompareTo(root.Value) > 0)
+                {
+                    if (root.RightChild != null) 
+                        AddNode(root.RightChild, nodeToAdd);
+                    else
+                    {
+                        root.RightChild = nodeToAdd;
+                        root.RightChild.Display("Right", root.Value);
+                    }
+                }
+            }
+        }
+
+        //public void DisplayTree(Node<T> root,int pad)
+        //{   
+            
+        //    string affichage = $"{root.Value}".PadLeft(pad);
+        //    string lienGauche = "/";
+        //    string lienDroit = "\\";
+        //    Console.Write(affichage);
+        //    Console.WriteLine();
+           
+        //    if(root.LeftChild != null && root.RightChild != null)
+        //    {
+        //        Console.Write(lienGauche.PadLeft(pad-1));
+        //        Console.Write(lienDroit);
+        //    }
+        //    Console.WriteLine();
+        //    if (root.LeftChild != null)
+        //    {
+               
+        //        DisplayTree(root.LeftChild, pad-4);
+        //    }
+        //    if (root.RightChild != null)
+        //    {
+                
+        //        DisplayTree(root.RightChild, pad+4);
+        //    }
+
+           
+        //}
+
+        public void PrefixSearch(Node<T> root)
+        {
+            Node<T> parent = root;
+            Console.Write($"{root.Value} - ");
+            if (root.LeftChild != null)
+            {
+                PrefixSearch(root.LeftChild);
+            }
+            if (root.RightChild != null)
+            {
+                PrefixSearch(root.RightChild);
+            }
+
+        }
+
+        public void InfixSearch(Node<T> root)
+        {
+            Node<T> parent = root;
+
+            if (root.LeftChild != null)
+            {
+                InfixSearch(root.LeftChild);
+            }
+            Console.Write($"{root.Value} - ");
+           
+
+            if (root.RightChild != null)
+            {
+                InfixSearch(root.RightChild);
+            }
+
+        }
+
+        public void PostfixSearch(Node<T> root)
+        {
+            if (root.LeftChild != null)
+            {
+                PostfixSearch(root.LeftChild);
+            }
+            if (root.RightChild != null)
+            {
+                PostfixSearch(root.RightChild);
+            }
+            Console.Write($"{root.Value} - ");
+        }
     }
 }
--- BinaryTreeProject/Node.cs
+++ BinaryTreeProject/Node.cs
@@ -6,21 +6,27 @@ using System.Threading.Tasks;
 
 namespace BinaryTreeProject
 {
-    public class Node<T>
+    public class Node<T> where T : IComparable
     {
+
+       
         public T Value { get; set; }
 
         public Node<T> LeftChild { get; set; }
 
         public Node<T> RightChild { get; set; }
 
-        public Node(T value, Node<T> node)
+        public Node(T value)
         {
             Value = value;
         }
-
-        
-
-
+         
+        public void Display(string prefixe, T parent)
+        {
+            
+            string message = $"Parent : {parent} --> {prefixe} : {Value} ";
+           
+            Console.WriteLine(message);
+        }
     }
 }
